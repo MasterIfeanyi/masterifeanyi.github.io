c332e03333afe374c7b8c18b9b31dda6c0e716ef
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2026-07-02 18:21:12 +0100

    update docusarus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,10 +21,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://masterifeanyi.github.io',
+  url: 'https://ifeanyi-techblog.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/my-mind/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
